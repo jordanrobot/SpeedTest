654e8eb8a66f490a56b2c94cf114f64a59258419
mjordan <meloncholia@gmail.com>
2020-04-06 08:41:26 -0400

    Added few comments, title, renamed module

--- src/SpeedTest Template Header.vb
+++ src/SpeedTest Template Header.vb
@@ -1,25 +1,28 @@
 '</CollectorHeader>
-Imports System.Text
+'### SpeedTest ###
+'
+'https://github.com/jordanrobot/SpeedTest
+'MIT license
 
-Module SpeedTestTemplateHeader '<CollectorPrepend>'</CollectorPrepend>
+Module SpeedTestMain '<CollectorPrepend>'</CollectorPrepend>
     Public Sub Main ()
         
         Dim test As SpeedTest = New SpeedTest()
 
-        test.AddTest("StringAppendTest", AddressOf TestOne)
-        test.AddTest("StringBuilderTest", AddressOf TestTwo)
-        test.Iterations = 50
+        test.AddTest("Test One", AddressOf TestOne)
+        test.AddTest("Test Two", AddressOf TestTwo)
+        test.Iterations = 500
         test.RunTests()
         test.ShowResultsInDialog()
 
     End Sub
 
     Public Sub TestOne()
- 
+        'Your code here!
     End Sub
 
     Public Sub TestTwo()
-
+        'Your code here!
     End Sub
 
-    End Module '<CollectorPrepend>'</CollectorPrepend>
+End Module '<CollectorPrepend>'</CollectorPrepend>
